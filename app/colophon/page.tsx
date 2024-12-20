57bc328ba0b68f0eebf29e3cdf14141e6cc7eeb2
Anton <anton.stallboerger@hfg-gmuend.de>
2024-12-20 12:27:43 +0100

    update: css now tailwind inline

--- app/colophon/page.tsx
+++ app/colophon/page.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next';
 
-import styles from '../../styles/views/colophon.module.css'
-
 export const metadata: Metadata = {
   title: 'Colophon'
 };
@@ -9,23 +7,23 @@ export const metadata: Metadata = {
 export default function ColophonPage() {
   return (
     <>
-      <header className={styles.header}>
-        <h1 className={styles.title}>Colophon</h1>
-        <p className={styles.date}>Last updated Jul 16, 2024</p>
+      <header className="col-span-full lg:col-span-8 md:col-span-12 lg:top-8 sticky lg:self-start mb-24">
+        <h1>Colophon</h1>
+        <p>Last updated Dec 20, 2024</p>
       </header>
 
-      <section className={styles.content}>
+      <section className="col-span-full md:col-span-12 lg:col-span-6">
         <h2>Typography</h2>
         <p>
-          <a href='https://klim.co.nz/retail-fonts/soehne/' className={styles.link} target="_blank" rel="noreferrer">Söhne</a> is a typeface designed and published by the <a href='https://klim.co.nz/' className={styles.link} target="_blank" rel="noreferrer">Klim Type Foundry</a>. It is the memory of Akzidenz-Grotesk framed through the reality of Helvetica. Söhne captures the analogue materiality of “Standard Medium” used in Unimark&rsquo;s legendary wayfinding system for the NYC Subway.
+          <a href='https://klim.co.nz/retail-fonts/soehne/' target="_blank" rel="noreferrer">Söhne</a> is a typeface designed and published by the <a href='https://klim.co.nz/' target="_blank" rel="noreferrer">Klim Type Foundry</a>. It is the memory of Akzidenz-Grotesk framed through the reality of Helvetica. Söhne captures the analogue materiality of “Standard Medium” used in Unimark&rsquo;s legendary wayfinding system for the NYC Subway.
         </p>
 
         <h2>Tech stack</h2>
         <p>
-          Built with <a href='https://nextjs.org/' className={styles.link} target="_blank" rel="noreferrer">Next.js</a> and <a href='https://tailwindcss.com/' className={styles.link} target="_blank" rel="noreferrer">Tailwind CSS</a>. Hosted on <a href='https://vercel.com/home' className={styles.link} target="_blank" rel="noreferrer">Vercel</a>. Privacy friendly analytics with <a href='https://plausible.io/' className={styles.link} target="_blank" rel="noreferrer">Plausible</a>.
+          Built with <a href='https://nextjs.org/' target="_blank" rel="noreferrer">Next.js</a> and <a href='https://tailwindcss.com/' target="_blank" rel="noreferrer">Tailwind CSS</a>. Hosted on <a href='https://vercel.com/home' target="_blank" rel="noreferrer">Vercel</a>. Privacy friendly analytics with <a href='https://plausible.io/' target="_blank" rel="noreferrer">Plausible</a>.
         </p>    
         <p>
-          <a href="https://github.com/antonstallboerger/antonstallboerger-v2" className={styles.link} target="_blank" rel="noreferrer">GitHub Repository</a>
+          <a href="https://github.com/antonstallboerger/antonstallboerger-v2" target="_blank" rel="noreferrer">GitHub Repository</a>
         </p>
           
         <h2>Photography</h2>
@@ -35,15 +33,14 @@ export default function ColophonPage() {
 
         <h2>Mockups</h2>
         <p>
-            All mockups are by <a href='https://www.pixelmator.com/pro/' className={styles.link} target="_blank" rel="noreferrer">Pixelmator Pro</a> or <a href='https://supply.family/' className={styles.link} target="_blank" rel="noreferrer">Supply Family</a>.
+          All mockups are by <a href='https://www.pixelmator.com/pro/' target="_blank" rel="noreferrer">Pixelmator Pro</a> or <a href='https://supply.family/' target="_blank" rel="noreferrer">Supply Family</a>.
         </p>
 
         <h2>Inspiration</h2>
         <ul>
           <li>
             <a 
-              href="https://linusrogge.com/" 
-              className={styles.link} 
+              href="https://linusrogge.com/"  
               target="_blank" 
               rel="noreferrer"
             >
@@ -51,54 +48,40 @@ export default function ColophonPage() {
             </a>
           </li>
           <li>
-              <a 
-                  href="https://sdrn.co/" 
-                  className={styles.link} 
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  sdrn.co
-              </a>
+            <a 
+              href="https://studiolenzing.com/"  
+              target="_blank" 
+              rel="noreferrer"
+            >
+              studiolenzing.com
+            </a>
           </li>
           <li>
-              <a 
-                  href="https://studiolenzing.com/" 
-                  className={styles.link} 
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  studiolenzing.com
-              </a>
+            <a 
+              href="https://emilkowal.ski/"  
+              target="_blank" 
+              rel="noreferrer"
+            >
+              emilkowal.ski
+            </a>
           </li>
           <li>
-              <a 
-                  href="https://emilkowal.ski/" 
-                  className={styles.link} 
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  emilkowal.ski
-              </a>
+            <a 
+              href="https://benji.org"  
+              target="_blank" 
+              rel="noreferrer"
+            >
+              benji.org
+            </a>
           </li>
           <li>
-              <a 
-                  href="https://benji.org" 
-                  className={styles.link} 
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  benji.org
-              </a>
-          </li>
-          <li>
-              <a 
-                  href="https://thu-le.com" 
-                  className={styles.link} 
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  thu-le.com
-              </a>
+            <a 
+              href="https://thu-le.com"  
+              target="_blank" 
+              rel="noreferrer"
+            >
+              thu-le.com
+            </a>
           </li>
         </ul>
 
@@ -108,54 +91,49 @@ export default function ColophonPage() {
         </p>
         <ul>
           <li>
-              <a 
-                  href="https://linusrogge.com/" 
-                  className={styles.link} 
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  Linus Rogge
-              </a>
+            <a 
+              href="https://linusrogge.com/"  
+              target="_blank" 
+              rel="noreferrer"
+            >
+              Linus Rogge
+            </a>
           </li>
           <li>
-              <a 
-                  href="https://floriankiem.com/" 
-                  className={styles.link}
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  Florian Kiem
-              </a>
+            <a 
+              href="https://floriankiem.com/" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              Florian Kiem
+            </a>
           </li>
           <li>
-              <a 
-                  href="https://www.nilseller.com/" 
-                  className={styles.link}
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  Nils Eller
-              </a>
+            <a 
+              href="https://nilseller.com/" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              Nils Eller
+            </a>
           </li>
           <li>
-              <a 
-                  href="https://leonardgorges.com/" 
-                  className={styles.link}
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  Leonard Gorges
-              </a>
+            <a 
+              href="https://leonardgorges.com/" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              Leonard Gorges
+            </a>
           </li>
           <li>
-              <a 
-                  href="https://www.henribredt.de/" 
-                  className={styles.link}
-                  target="_blank" 
-                  rel="noreferrer"
-              >
-                  Henri Bredt
-              </a>
+            <a 
+              href="https://henribredt.de/" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              Henri Bredt
+            </a>
           </li>
         </ul>
       </section>
