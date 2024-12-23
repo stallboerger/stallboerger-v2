86a3e80e99819bcd52759cc310909af46dd521ed
Anton <anton.stallboerger@hfg-gmuend.de>
2024-12-23 13:30:38 +0100

    update: tailwind inline

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import Image from 'next/image';
 
-import styles from '../../styles/views/about.module.css'
 import { format, isAfter } from 'date-fns';
 import LiteralBooks from '@/components/books';
 
@@ -56,45 +55,45 @@ const experiences: Experience[] = [
 export default function AboutPage() {
 	return (
 	<>
-		<figure className={styles.figure}>
+		<figure className="sm:mt-[calc(-180px_+_53px+32px)] col-span-4 aspect-[4_/_5] bg-sand-2 dark:bg-sand-dark-2 mt-[calc(-140px_+_53px_+_16px)]">
 			<Image 
 				src={`/images/about/anton-1.jpeg`}
 				alt={`Anton Stallbörger`}
 				width="1920" 
 				height="305"
-				className={styles.image} />
+				className="w-full h-full object-cover" />
 		</figure>
-		<figure className={styles.figure}>
+		<figure className="sm:mt-[calc(-180px_+_53px+32px)] col-span-4 aspect-[4_/_5] bg-sand-2 dark:bg-sand-dark-2 mt-[calc(-140px_+_53px_+_16px)]">
 			<Image 
 				src={`/images/about/anton-hamburg-shoes.jpeg`}
 				alt={`Anton Stallbörger`}
 				width="1920" 
 				height="305"
-				className={styles.image} />
+				className="w-full h-full object-cover" />
 		</figure>
-		<figure className={styles.figure}>
+		<figure className="sm:mt-[calc(-180px_+_53px+32px)] col-span-4 aspect-[4_/_5] bg-sand-2 dark:bg-sand-dark-2">
 			<Image 
 				src={`/images/about/anton-close-up-black-and-white.jpg`}
 				alt={`Anton Stallbörger`}
 				width="1920" 
 				height="305"
-				className={styles.image} />
+				className="w-full h-full object-cover" />
 		</figure>
-		<figure className={styles.figure}>
+		<figure className="sm:mt-[calc(-180px_+_53px+32px)] col-span-4 aspect-[4_/_5] bg-sand-2 dark:bg-sand-dark-2">
 			<Image 
 				src={`/images/about/anton-flims-gelbes-haus.jpeg`}
 				alt={`Anton Stallbörger`}
 				width="1920" 
 				height="305"
-				className={styles.image} />
+				className="w-full h-full object-cover" />
 		</figure>
 
-		<header className={styles.header}>
-			<h1 className={styles.title}>About Anton</h1>
+		<header className="col-span-full md:col-span-12 lg:col-span-8 lg:sticky lg:top-8 lg:self-start mb-20 lg:mb-0">
+			<h1>About Anton</h1>
 			<p>B. 2002</p>
 		</header>
 
-		<section className={styles.content}>
+		<section className="col-span-full md:col-span-12 lg:col-span-7 mb-20">
 			<p>
 				I am a passionate designer and developer who is driven by a love for great, high-quality and thoughtful design. Design is an integral part of my everyday life, and I believe that it has the power to shape and improve the world around us.
 			</p>
@@ -103,20 +102,20 @@ export default function AboutPage() {
 			</p>
 		</section>
 
-		<section className={styles.like}>
+		<section className="col-span-4 sm:col-span-8 lg:col-span-4 border-t border-t-sand-6 dark:border-t-sand-dark-6 pt-8 lg:col-start-9">
 			<h2>Things I like</h2>
 			<ul>
-				<li><a href='https://www.cosmos.so/stallboerger/goods' className={styles.link} target='_blank' rel="noreferrer">Quality goods</a></li>
+				<li><a href='https://www.cosmos.so/stallboerger/goods' target='_blank' rel="noreferrer">Quality goods</a></li>
 				<li>Wearing black</li>
-				<li><a href='https://music.apple.com/de/playlist/con-amore/pl.u-r2yBDE4tdN97Zo?l=en-GB' className={styles.link} target='_blank' rel="noreferrer">Piano music</a></li>
+				<li><a href='https://music.apple.com/de/playlist/con-amore/pl.u-r2yBDE4tdN97Zo?l=en-GB' target='_blank' rel="noreferrer">Piano music</a></li>
 				<li>Good food</li>
 				<li>Mountains</li>
-				<li><a href='https://www.cosmos.so/stallboerger/architecture' className={styles.link} target='_blank' rel="noreferrer">Architecture</a></li>
-				<li><a href='https://www.cosmos.so/stallboerger/interior' className={styles.link} target='_blank' rel="noreferrer">Interior Design</a></li>
-				<li><a href='https://www.cosmos.so/stallboerger/concrete' className={styles.link} target='_blank' rel="noreferrer">Concrete</a></li>
+				<li><a href='https://www.cosmos.so/stallboerger/architecture' target='_blank' rel="noreferrer">Architecture</a></li>
+				<li><a href='https://www.cosmos.so/stallboerger/interior' target='_blank' rel="noreferrer">Interior Design</a></li>
+				<li><a href='https://www.cosmos.so/stallboerger/concrete' target='_blank' rel="noreferrer">Concrete</a></li>
 			</ul>
 		</section>
-		<section className={styles.dislike}>
+		<section className="col-span-4 sm:col-span-8 lg:col-span-4 border-t border-t-sand-6 dark:border-t-sand-dark-6 pt-8">
 			<h2>Things I don’t like</h2>
 			<ul>
 				<li>Smalltalk</li>
@@ -133,21 +132,21 @@ export default function AboutPage() {
 			<LiteralBooks customClass='col-span-8' />
 		</section>
 
-		<section className={styles.experience}>
-			<h2 className={styles.title}>Experience</h2>
+		<section className='col-span-full lg:col-span-12 lg:col-start-5 border-t border-t-sand-6 dark:border-t-sand-dark-6 pt-8 mt-20 grid grid-cols-16 lg:grid-cols-12 gap-4 md:gap-8 md:gap-y-8'>
+			<h2 className='col-span-8 lg:col-span-4'>Experience</h2>
 
 			{experiences.map((experience, key) => (
-				<div className={styles.position} key={key}>
-					<div className={styles.meta}>
-						<h3 className={styles.title}>
+				<div className='col-span-full md:col-span-8 md:col-start-9 lg:col-start-5 grid grid-cols-8 gap-4 not-last:border-b not-last:border-sand-6 not-last:dark:border-sand-dark-6 not-last:pb-12 not-last:md:pb-20' key={key}>
+					<div className='col-span-full'>
+						<h3 className='col-span-4 font-semibold'>
 							{
 								experience.url
-								? <a href={`${experience.url}`} target='_blank' className={styles.link}>{experience.role && `${experience.role} at`} {experience.company}</a>
+								? <a href={`${experience.url}`} target='_blank'>{experience.role && `${experience.role} at`} {experience.company}</a>
 								: <>{experience.role && `${experience.role} at`} {experience.company}</>
 							}
 							
 						</h3>
-						<span className={styles.timeframe}>
+						<span className='block'>
 							{
 								(experience.end && isAfter(experience.end, new Date())) || !experience.end
 								? `${format(experience.start, 'LLLL, yyyy')} – Ongoing`
@@ -156,7 +155,7 @@ export default function AboutPage() {
 						</span>
 					</div>
 
-					{experience.description && <p className={styles.description}>{experience.description}</p>}
+					{experience.description && <p className='mt-4 col-span-full sm:col-span-6 max-w-none'>{experience.description}</p>}
 				</div>
 			))}
 
