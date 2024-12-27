c0f69ca3351410ef2caec4d4c5e5a6f1bb717c47
Anton <anton.stallboerger@hfg-gmuend.de>
2024-12-27 14:29:56 +0100

    fix: focus state

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -53,7 +53,7 @@ export default function Navbar() {
   return (
     <nav className='grid-cols-16 grid gap-4 lg:gap-8 justify-between items-center w-full h-[53px] z-50 px-4 lg:px-8 bg-sand-1 dark:bg-sand-dark-1 select-none'>
       <Link href='/' className={clsx(
-        'col-span-3',
+        'col-span-3 justify-self-start',
         'text-sand-11 dark:text-sand-dark-11 no-underline',
         currentRoute === '/' && 'text-sand-12 dark:text-sand-dark-12',
       )}>
@@ -64,7 +64,6 @@ export default function Navbar() {
         {items.map((item) => (
           <Link key={item.title} href={item.href} className={clsx(
             'text-sand-11 dark:text-sand-dark-11 no-underline',
-            item.href === '/' && 'col-span-3',
             currentRoute === item.href && 'text-sand-12 dark:text-sand-dark-12',
           )}>{item.title}</Link>
         ))}
