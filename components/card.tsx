b8455a03250221f78e4e43ca82d7fe033aaa1b9b
Anton <anton.stallboerger@hfg-gmuend.de>
2024-12-27 14:29:31 +0100

    update: focus style

--- components/card.tsx
+++ components/card.tsx
@@ -25,26 +25,27 @@ const Card: React.FC<Props> = ({
         <time>{year}</time>
       </div>
     
-      <div className="bg-white dark:bg-black shadow-[0_0_0_1px_rgba(0,0,0,0.08)] dark:shadow-[0_0_0_1px_rgba(255,255,255,0.08)] overflow-hidden mb-8">
-        <Link href={link}>
-          <Image
-            src={imageSrc}
-            alt={imageAlt}
-            className="select-none"
-            width={1080}
-            height={1080}
-            sizes="(max-width: 720px) 80vw, (max-width: 1080px) 50vw, 40vw"
-            priority
-          />
-        </Link>
-      </div>
+      <Link 
+        href={link}
+        className="block bg-white dark:bg-black shadow-[0_0_0_1px_rgba(0,0,0,0.08)] dark:shadow-[0_0_0_1px_rgba(255,255,255,0.08)] mb-8 ring-[#008CFF] ring-offset-2 focus-visible:ring-2"
+      >
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          className="select-none"
+          width={1500}
+          height={1500}
+          sizes="(max-width: 720px) 80vw, (max-width: 1080px) 50vw, 40vw"
+          priority
+        />
+      </Link>
       
       <div className="grid grid-cols-8 gap-8 mb-12 lg:mb-24">
         <p className="col-span-full lg:col-span-6">{description}</p>
         
         <Link 
           href={link}
-          className="col-span-full lg:col-span-2 lg:justify-self-end"
+          className="col-span-full lg:col-span-2 lg:justify-self-end self-start"
           title={`Learn more about ${title}`}
         >
           Learn more
